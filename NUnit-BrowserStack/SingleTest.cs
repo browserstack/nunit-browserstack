95e5fde77d02b47c607568c1d7d368c6d7a5fe11
Rutvik Chandla <97675949+RutvikChandla@users.noreply.github.com>
2022-04-17 22:01:11 +0530

    Change script to bstack demo

--- NUnit-BrowserStack/SingleTest.cs
+++ NUnit-BrowserStack/SingleTest.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using OpenQA.Selenium;
 
 namespace BrowserStack
@@ -9,14 +9,18 @@ namespace BrowserStack
     public SingleTest(string profile, string environment) : base(profile, environment) { }
 
     [Test]
-    public void SearchGoogle()
+    public void BstackDemo()
     {
-      driver.Navigate().GoToUrl("https://www.google.com/ncr");
-      IWebElement query = driver.FindElement(By.Name("q"));
-      query.SendKeys("BrowserStack");
-      query.Submit();
-      System.Threading.Thread.Sleep(5000);
-      Assert.AreEqual("BrowserStack - Google Search", driver.Title);
+      driver.Navigate().GoToUrl("https://bstackdemo.com/");
+      Assert.AreEqual("StackDemo", driver.Title);
+
+      string productOnPageText = driver.FindElement(By.XPath("//*[@id=\"1\"]/p")).Text;
+
+      driver.FindElement(By.XPath("//*[@id=\"1\"]/div[4]")).Click();
+      bool cartOpened = driver.FindElement(By.XPath("//*[@class=\"float-cart__content\"]")).Displayed;
+      Assert.AreEqual(cartOpened, true);
+      string productOnCartText = driver.FindElement(By.XPath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).Text;
+      Assert.AreEqual(productOnCartText, productOnPageText);
     }
   }
 }
